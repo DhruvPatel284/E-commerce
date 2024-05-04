235a0b7e9f346ba5f332fd8dc4b90deafddde320
DhruvPatel284 <dhruv156328@gmail.com>
2024-05-04 13:39:04 +0530

    order routes added

---
+++ src/app/api/order/cancelOrder/[orderId]/order.ts
@@ -0,0 +1,23 @@
+import prisma from "@/lib/prismadb";
+import { NextResponse } from "next/server";
+
+export async function POST(
+  req: Request,
+  context: { params: { orderid: string } }
+) {
+  try {
+    const { body } = await req.json();
+    const order = await prisma.order.update({
+      where: { id: context.params.orderid },
+      data: body,
+    });
+
+    if (order) {
+      return NextResponse.json(order);
+    } else {
+      return new NextResponse("Order not found", { status: 401 });
+    }
+  } catch (error) {
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
