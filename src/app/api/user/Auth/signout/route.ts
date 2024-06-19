2e579007491985a9b408ed5780e0227ae6d14606
DhruvPatel284 <dhruv156328@gmail.com>
2024-06-19 12:12:07 +0530

    errors 9

--- src/app/api/user/Auth/signout/route.ts
+++ src/app/api/user/Auth/signout/route.ts
@@ -1,12 +1,29 @@
-import { NextResponse } from "next/server";
-import { cookies } from "next/headers";
-export async function GET(req: Request) {
+// import { NextResponse } from "next/server";
+// import { cookies } from "next/headers";
+// export async function GET(req: Request) {
+//   try {
+//     const response = new NextResponse("Logout successful");
+//     cookies().delete("token");
+//     return response;
+//   } catch (error) {
+//     console.log(error);
+//     return new NextResponse("Internal Error", { status: 500 });
+//   }
+// }
+
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
   try {
-    const response = new NextResponse("Logout successful");
-    cookies().delete("token");
+    // Create a new response object
+    const response = NextResponse.json({ message: "Logout successful" });
+
+    // Delete the "token" cookie
+    response.cookies.set("token", "", { expires: new Date(0) });
+
     return response;
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
