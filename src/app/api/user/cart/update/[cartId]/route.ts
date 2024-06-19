ec24851ebe096264ddbf7762a3e16ede935ccc5a
DhruvPatel284 <dhruv156328@gmail.com>
2024-06-19 15:23:19 +0530

    id problems

--- src/app/api/user/cart/update/[cartId]/route.ts
+++ src/app/api/user/cart/update/[cartId]/route.ts
@@ -3,10 +3,10 @@ import { NextResponse ,NextRequest} from "next/server";
 
 export async function POST(
   req: NextRequest,
-  context: { params: { userId: string } }
+  context: { params: { cartId: string } }
 ) {
   //const cartId = req.nextUrl.searchParams.get("cartId");
-  const cartId = context.params.userId
+  const cartId = context.params.cartId
   if (cartId === null) {
     return new NextResponse("CartId parameter is missing", { status: 400 });
   }
