67d5452fa645f025bfbf67670dde03c6e0eb135e
DhruvPatel284 <dhruv156328@gmail.com>
2024-06-21 16:43:53 +0530

    search bar updated

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -49,20 +49,20 @@ const Search = () => {
       }, [searchTerm]);
 
   return(
-        <div className="w-[900px]">
+        <div className="w-[900px] ">
             <div className="flex">
                 <label htmlFor="search-dropdown" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your Email</label>
-                <select className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-slate-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600">
-                    <option className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                <select className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-slate-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100">
+                    <option className="inline-flex w-full px-4 py-2 hover:bg-gray-100">
                         Trending Now
                     </option>
-                    <option className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                    <option className="inline-flex w-full px-4 py-2 hover:bg-gray-100 ">
                         Deals
                     </option>
-                    <option className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                    <option className="inline-flex w-full px-4 py-2 hover:bg-gray-100 ">
                         Fashion
                     </option>
-                    <option className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                    <option className="inline-flex w-full px-4 py-2 hover:bg-gray-100">
                         Electronics
                     </option>  
                 </select>
@@ -71,7 +71,7 @@ const Search = () => {
                     onChange={(e) => {
                         setSearchTerm(e.target.value);
                     }} value={searchTerm}
-                    className="block h-[40px] p-2.5 w-full z-20 text-md font-semibold text-gray-950 bg-slate-100 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search What you Want.." required />
+                    className="block h-[40px] p-2.5 w-full z-20 text-md font-semibold text-gray-950 bg-slate-100 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Search What you Want.." required />
                     <button type="submit" onClick={handleSearch} className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white">
                         <MagnifyingGlassIcon className="h-[27px] m-auto stroke-slate-900" />
                     </button>
